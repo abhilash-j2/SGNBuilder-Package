8e5855fb7050ece950b45812aa1aab5aa946c8c3
Abhilash Janardhanan <Abhilash.J@hotmail.com>
2021-05-14 11:33:14 -0400

    fix error with requirements parsing

--- setup.py
+++ setup.py
@@ -4,9 +4,6 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
-links = []
-requires = []
-
 try:
     # pip >=20
     from pip._internal.network.session import PipSession
@@ -29,21 +26,11 @@ except:
     requirements = parse_requirements(
         'requirements.txt', session=PipSession())
 
-for item in requirements:
-    # we want to handle package names and also repo urls
-    if getattr(item, 'url', None):  # older pip has url
-        links.append(str(item.url))
-    if getattr(item, 'link', None): # newer pip has link
-        links.append(str(item.link))
-    if item.req:
-        requires.append(str(item.req))
-
-
 setup(
     name="SGNBuilder",
     version="0.1.1",
     url='https://github.com/abhilash-j2/SGNBuilder-Package',
-    install_requires=requirements,
+    install_requires=[str(requirement.requirement) for requirement in requirements],
     packages=find_packages(),
     include_package_data=True,
     author="Abhilash Janardhanan",
