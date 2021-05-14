d60340bf82875df80b0df016afb5886926d4adb4
Abhilash Janardhanan <Abhilash.J@hotmail.com>
2021-05-14 11:16:48 -0400

    Added requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="SGNBuilder",
     version="0.1.0",
+    install_requires=requirements,
     packages=find_packages(),
     author="Abhilash Janardhanan",
     author_email="jabhilash7@gmail.com"
