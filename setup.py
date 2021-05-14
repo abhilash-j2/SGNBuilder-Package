32f3537b298dacd61a387a31f68f4bd31f4349fa
Abhilash Janardhanan <Abhilash.J@hotmail.com>
2021-05-14 11:27:25 -0400

    Accounting for different pip versions

--- setup.py
+++ setup.py
@@ -8,11 +8,26 @@ links = []
 requires = []
 
 try:
-    requirements = pip.req.parse_requirements('requirements.txt')
+    # pip >=20
+    from pip._internal.network.session import PipSession
+    from pip._internal.req import parse_requirements
+except ImportError:
+    try:
+        # 10.0.0 <= pip <= 19.3.1
+        from pip._internal.download import PipSession
+        from pip._internal.req import parse_requirements
+    except ImportError:
+        # pip <= 9.0.3
+        from pip.download import PipSession
+        from pip.req import parse_requirements
+
+
+try:
+    requirements = parse_requirements('requirements.txt')
 except:
     # new versions of pip requires a session
-    requirements = pip.req.parse_requirements(
-        'requirements.txt', session=pip.download.PipSession())
+    requirements = parse_requirements(
+        'requirements.txt', session=PipSession())
 
 for item in requirements:
     # we want to handle package names and also repo urls
